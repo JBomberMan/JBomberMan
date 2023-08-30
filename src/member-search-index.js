38deb99a429bfa397785de8cb347cb3a2ca69545
alessio <alessio.taruffi2020@gmail.com>
2023-08-30 12:58:02 +0200

    nuovo javadoc

--- src/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"<Unnamed>","c":"JBomberMan","l":"JBomberMan()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"JBomberMan","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
